4ea3c366e3c46cfd2bdd6cea76134e8e25070796
VikyRa <57253332+VikyRa@users.noreply.github.com>
2021-10-23 16:01:53 +0530

    Update urlConfig.js

--- admin/src/urlConfig.js
+++ admin/src/urlConfig.js
@@ -1,4 +1,4 @@
-export const api = 'http://127.0.0.1:8000/api/';
+export const api = 'https://gurudevadmin.herokuapp.com:4000/api/';
 export const gernateImages =(img)=>{
-    return `http://127.0.0.1:8000${img}`;
+    return `https://gurudevadmin.herokuapp.com:4000${img}`;
 };
